61002251cd101de6046773b4b4a69dd9e0435b87
MariusLastauskas <marius20122@gmail.com>
2019-05-18 12:39:25 +0300

    lexem spamishness

--- sarkazmas/sarkazmas.py
+++ sarkazmas/sarkazmas.py
@@ -1,8 +1,17 @@
 import json
 
+class Article:
+    def __init__(self, headline, is_sarcastic):
+        self.headline = headline
+        self.is_sarcastic = is_sarcastic
+
+    def __str__(self):
+        return "Sarcastic: {0}, Headline: {1}".format(self.is_sarcastic, self.headline)
+
 def data_prep(dir_path, exit_path):
     open(exit_path, 'w').close()
     fout = open(exit_path, 'a')
+    print(dir_path)
     fin = open(dir_path, 'r')
 
     fout.write('{"sarkazmas" : [')
@@ -20,14 +29,57 @@ def data_prep(dir_path, exit_path):
     fout.close()
     fin.close()
 
+def get_lexem_spam_name(sarcastic_lex, non_sarcastic_lex):
+    n_sarc = 0
+    n_non_sarc = 0
+    for value in sarcastic_lex.values():
+        sarcastic_n = n_sarc + value['count']
+    for value in non_sarcastic_lex.values():
+        non_sarcastic_n = n_non_sarc + value['count']
+
+    sarcasm_lvl = {}
+    for key in sarcastic_lex.keys():
+        try:
+            pSar = sarcastic_lex[key] / n_sarc
+            pNonSar = non_sarcastic_lex[key] / n_non_sarc
+            p = pSar / (pSar / pNonSar)
+            sarcasm_lvl[key] = p
+        except KeyError:
+            sarcasm_lvl[key] = 0.99
+
+    for key in non_sarcastic_lex.key():
+        try:
+            sarcasm_lvl[key]
+        except KeyError:
+            try:
+                pSar = sarcastic_lex[key] / n_sarc
+                pNonSar = sarcastic_lex[key] / n_non_sarc
+                p = pSar / (pSar / pNonSar)
+                sarcasm_lvl[key] = p
+            except KeyError:
+                sarcasm_lvl = 0.01
+
+    return sarcasm_lvl
+
+# def test_sarcasm(sarcastic_articles, non_sarcastic_articles):
+#
+#     for i in range(n_cross_validation):
+
+
 if __name__ == '__main__':
     raw_data = 'Sarcasm_Headlines_Dataset.json'
     prep_data = 'sarcasm_prepaired.json'
     data_prep(raw_data, prep_data)
+    articles = {}
+    n_cross_validation = 10
+
     with open(prep_data) as json_file:
         data = json.load(json_file)
+        i = 0
         for p in data['sarkazmas']:
-            print('article_link: ' + p['article_link'])
-            print('headline: ' + p['headline'])
-            print('is_sarcastic: ' + str(p['is_sarcastic']))
-            print('')
+            d = Article(p['headline'], p['is_sarcastic'])
+            articles[i] = d
+            i = i + 1
+        # print(articles[5])
+
+    for i in range(n)
