4259314352c7d6e9c118a343e4569e498dc16b87
MariusLastauskas <marius20122@gmail.com>
2019-05-18 16:28:13 +0300

    fixes

--- sarkazmas/sarkazmas.py
+++ sarkazmas/sarkazmas.py
@@ -9,6 +9,7 @@ MODIFIED_DATA_FILE = 'sarcasm_prepaired.json'
 LEXEM_COUNT = 20
 SARCASM_BORDER = 0.5
 
+
 class Article:
     def __init__(self, headline, is_sarcastic, article_link):
         self.headline = headline
@@ -19,6 +20,7 @@ class Article:
         return "Sarcastic: {0}, Headline: {1}".format(self.is_sarcastic, self.headline)
 
     __repr__ = __str__
+
 
 def data_prep(dir_path, exit_path):
     open(exit_path, 'w').close()
@@ -41,6 +43,7 @@ def data_prep(dir_path, exit_path):
     fout.close()
     fin.close()
 
+
 def get_lexem_sarcasm_lvl(sarcastic_lex, non_sarcastic_lex):
     n_sarc = 0
     n_non_sarc = 0
@@ -51,7 +54,7 @@ def get_lexem_sarcasm_lvl(sarcastic_lex, non_sarcastic_lex):
 
     sarcasm_lvl = {}
     for key in sarcastic_lex.keys():
-        key = key
+        key = key.lower()
         try:
             pSar = sarcastic_lex[key] / n_sarc
             pNonSar = non_sarcastic_lex[key] / n_non_sarc
@@ -61,7 +64,7 @@ def get_lexem_sarcasm_lvl(sarcastic_lex, non_sarcastic_lex):
             sarcasm_lvl[key] = 0.99
 
     for key in non_sarcastic_lex.keys():
-        key = key
+        key = key.lower()
         try:
             sarcasm_lvl[key]
         except KeyError:
@@ -77,18 +80,23 @@ def get_lexem_sarcasm_lvl(sarcastic_lex, non_sarcastic_lex):
 
     return sarcasm_lvl
 
+
 def read_data(prep_data):
     with open(prep_data) as json_file:
         data = json.load(json_file)
-        mappedData = list(map(lambda p: Article(p['headline'].lower(), p['is_sarcastic'], p['article_link']), data['sarkazmas']))
+        mappedData = list(
+            map(lambda p: Article(p['headline'], p['is_sarcastic'], p['article_link']), data['sarkazmas']))
         return mappedData
 
+
 def get_separated_articles(parsed_data):
-    return list(filter(lambda x: x.is_sarcastic == 1, parsed_data)), list(filter(lambda x: x.is_sarcastic == 0, parsed_data))
+    return list(filter(lambda x: x.is_sarcastic == 1, parsed_data)), list(
+        filter(lambda x: x.is_sarcastic == 0, parsed_data))
+
 
 def get_lex(articles):
     lex = dict()
-    
+
     for article in articles:
         words = re.split(r'\W+', article.headline)
 
@@ -100,6 +108,7 @@ def get_lex(articles):
 
     return lex
 
+
 def test_data(art, isSarc, lexem_sarcasm_lvl):
     correct = 0
 
@@ -109,7 +118,7 @@ def test_data(art, isSarc, lexem_sarcasm_lvl):
         for word in words:
             if word == '':
                 continue
-            word = word
+            word = word.lower()
             try:
                 x = lexem_sarcasm_lvl[word]
             except KeyError:
@@ -145,10 +154,11 @@ def test_data(art, isSarc, lexem_sarcasm_lvl):
                 correct = correct + 1
     return correct
 
+
 def get_urls(articles):
     urls = dict()
-    
-    for article in articles:    
+
+    for article in articles:
         if article.article_link in urls:
             urls[article.article_link] += 1
         else:
@@ -156,14 +166,17 @@ def get_urls(articles):
 
     return urls
 
+
 def filter_lex(sarcastic_lex, not_sarcastic_lex, minimum_count):
-    return dict(filter(lambda x: x[1] > minimum_count or x[0] in not_sarcastic_lex, sarcastic_lex.items())), dict(filter(lambda x: x[1] > minimum_count or x[0] in sarcastic_lex, not_sarcastic_lex.items()))
+    return dict(filter(lambda x: x[1] > minimum_count or x[0] in not_sarcastic_lex, sarcastic_lex.items())), dict(
+        filter(lambda x: x[1] > minimum_count or x[0] in sarcastic_lex, not_sarcastic_lex.items()))
+
 
 if __name__ == '__main__':
     data_prep(RAW_DATA_FILE, MODIFIED_DATA_FILE)
     parsed_data = read_data(MODIFIED_DATA_FILE)
 
-    sarcastic_articles, not_sarcastic_articles = get_separated_articles(parsed_data[:len(parsed_data)*9//10])
+    sarcastic_articles, not_sarcastic_articles = get_separated_articles(parsed_data[:len(parsed_data) * 9 // 10])
 
     print(get_urls(sarcastic_articles))
     print(get_urls(not_sarcastic_articles))
@@ -171,7 +184,7 @@ if __name__ == '__main__':
     sarcastic_lex = get_lex(sarcastic_articles)
     not_sarcastic_lex = get_lex(not_sarcastic_articles)
 
-    #Duomenu pasifiltravimui, jei nenorima, jog labai mazo kiekio leksemos, esancios tik vienoje leksemu puseje, neisdarkytu rezultatu
+    # Duomenu pasifiltravimui, jei nenorima, jog labai mazo kiekio leksemos, esancios tik vienoje leksemu puseje, neisdarkytu rezultatu
     f_slex, f_nslex = filter_lex(sarcastic_lex, not_sarcastic_lex, 1)
     print(f_slex)
     print(f_nslex)
@@ -181,7 +194,7 @@ if __name__ == '__main__':
 
     # lexem_sarcasm_lvl = sorted(lexem_sarcasm_lvl.items(), key=operator.itemgetter(1))
 
-    sarcastic_test_data, not_sarcastic_test_data = get_separated_articles(parsed_data[len(parsed_data)*9//10:])
+    sarcastic_test_data, not_sarcastic_test_data = get_separated_articles(parsed_data[len(parsed_data) * 9 // 10:])
 
     print(test_data(sarcastic_test_data, 1, lexem_sarcasm_lvl) / len(sarcastic_test_data))
     print(test_data(not_sarcastic_test_data, 0, lexem_sarcasm_lvl) / len(not_sarcastic_test_data))
